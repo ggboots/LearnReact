a889da86bf6c5528af3ef36b052b36504e66b656
George Boots <ggboots.99@gmail.com>
2022-05-15 07:04:03 +1000

    next-app

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-
-// import {App} from 'index.js';
-
-// const root = document.getElementById('root');
-
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-// ReactDOM.render(<h1>testing</h1>,document.getElementById('root'))
-
-// http://cheng.logdown.com/posts/2016/03/25/679045
